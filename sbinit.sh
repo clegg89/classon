e2525d7676f50678c9c264d0002a70dbd2c2c427
Matt Hilt <matthew@matthewhilt.com>
2017-04-09 12:21:25 -0400

    Recursive submodule branch checkout

--- sbinit.sh
+++ sbinit.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # Make sure sandbox and environment are setup
 
-#git submodule update --init --recursive
-git submodule update --recursive
+git submodule update --init --recursive
+#git submodule update --recursive
+
+git submodule foreach --recursive 'git checkout $(git config -f $toplevel/.gitmodules submodule.$name.branch || echo master)'
 
 if [ -e ./repo-deps/openembedded-core/bitbake ]; then
   echo "Bitbake symlink OK"
